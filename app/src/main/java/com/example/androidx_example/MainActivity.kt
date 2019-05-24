caeb39cede31f05b5095dc9714fc6433c49e2512
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2019-05-24 22:50:23 +0800

    update side navigation

--- app/src/main/java/com/example/androidx_example/MainActivity.kt
+++ app/src/main/java/com/example/androidx_example/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.androidx_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.example.androidx_example.until.debugInfo
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,16 @@ class MainActivity : AppCompatActivity() {
         initNav()
     }
 
-    fun initNav() {
-        val navCtrl = nav_host.findNavController()
+    private fun initNav() {
+        val navCtrl = nav_host.findNavController().apply {
+            addOnDestinationChangedListener { _, destination, _ ->
+                navigation_side?.setCheckedItem(destination.id)
+            }
+        }
         main_bottom_navigation.setupWithNavController(navCtrl)
+        navigation_side.apply {
+            setupWithNavController(navCtrl)
+            itemIconTintList = null
+        }
     }
 }
