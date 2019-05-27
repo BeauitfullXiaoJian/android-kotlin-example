c99009d5e4d65cfd07dac4910a314ebfa8b4a3f6
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2019-05-27 10:17:00 +0800

    clean project

--- app/src/main/java/com/example/androidx_example/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/androidx_example/fragments/BaseFragment.kt
@@ -2,9 +2,6 @@ package com.example.androidx_example.fragments
 
 import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
